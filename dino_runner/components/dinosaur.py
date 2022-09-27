d18e502522572dc95e9f0a61c7f451a42a200e0d
Hugo Souza <hugoedson2019@gmail.com>
2022-09-26 21:08:19 -0300

    Duck function added
    finally got duck function working, added basically the run function code, changed the image and changed the dino_react (there's a mistype in the code)  to 350 to fix the dino walking in the air.

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,5 +1,5 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import DUCKING, RUNNING, JUMPING
 
 
 class Dinosaur:
@@ -14,6 +14,7 @@ class Dinosaur:
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
         
     def update(self, user_input):
@@ -21,6 +22,8 @@ class Dinosaur:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
             
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -28,7 +31,12 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
-        
+            
+        if user_input[pygame.K_DOWN]:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
+            
         if self.step_index >= 10:
             self.step_index = 0
     
@@ -51,7 +59,11 @@ class Dinosaur:
             self.jump_vel = self.JUMP_VEL
             
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = 350
+        self.step_index += 1
             
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
