1475ad6df4f28ba5aa7d7529bd2683e33f397f06
Hugo Souza <hugoedson2019@gmail.com>
2022-09-28 10:01:44 -0300

    Large Cactus (atividade)
    Adicionado o objeto Large Cactus, porém achei que a área de contato dos cactus está muito grande, ainda não sei como alterar isso.

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,8 @@
 import pygame
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.largecactus import LargeCactus
 from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS
 
 
 class ObstacleManager:
@@ -10,6 +12,7 @@ class ObstacleManager:
     def update(self, game):
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(LargeCactus(LARGE_CACTUS))
             
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
